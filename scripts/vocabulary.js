0cf70dc5708d76384d48f2c1db5fa553ce12fa69
santiago <munozuribes73@gmail.com>
2024-11-21 17:17:31 -0500

    first commit

---
+++ scripts/vocabulary.js
@@ -0,0 +1,177 @@
+const vocabulary = [
+    {
+      id: 1,
+      word: 'apple',
+      translation: 'manzana',
+      example: 'I eat an apple every day.',
+      image: './assets/images/apple.png'
+    },
+    {
+      id: 2,
+      word: 'house',
+      translation: 'casa',
+      example: 'This is my new house.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 3,
+      word: 'book',
+      translation: 'libro',
+      example: 'She is reading a book.',
+      image: './assets/images/book.png'
+    },
+    {
+      id: 4,
+      word: 'car',
+      translation: 'coche',
+      example: 'I drive my car to work.',
+      image: './assets/images/car.png'
+    },
+    {
+      id: 5,
+      word: 'dog',
+      translation: 'perro',
+      example: 'The dog is barking loudly.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 6,
+      word: 'school',
+      translation: 'escuela',
+      example: 'We go to school every day.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 7,
+      word: 'water',
+      translation: 'agua',
+      example: 'Drink water to stay hydrated.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 8,
+      word: 'friend',
+      translation: 'amigo',
+      example: 'My friend is very kind.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 9,
+      word: 'computer',
+      translation: 'computadora',
+      example: 'I use a computer to write code.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 10,
+      word: 'music',
+      translation: 'música',
+      example: 'I love listening to music.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 11,
+      word: 'cat',
+      translation: 'gato',
+      example: 'The cat is sleeping on the couch.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 12,
+      word: 'table',
+      translation: 'mesa',
+      example: 'Put the keys on the table.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 13,
+      word: 'chair',
+      translation: 'silla',
+      example: 'I bought a new chair for my office.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 14,
+      word: 'light',
+      translation: 'luz',
+      example: 'Turn off the light before leaving.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 15,
+      word: 'food',
+      translation: 'comida',
+      example: 'This restaurant serves delicious food.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 16,
+      word: 'city',
+      translation: 'ciudad',
+      example: 'New York is a big city.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 17,
+      word: 'window',
+      translation: 'ventana',
+      example: 'Open the window for fresh air.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 18,
+      word: 'phone',
+      translation: 'teléfono',
+      example: 'My phone battery is low.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 19,
+      word: 'tree',
+      translation: 'árbol',
+      example: 'The tree in the park is very old.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 20,
+      word: 'train',
+      translation: 'tren',
+      example: 'We took the train to the city center.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 21,
+      word: 'paper',
+      translation: 'papel',
+      example: 'Please hand me a sheet of paper.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 22,
+      word: 'flower',
+      translation: 'flor',
+      example: 'She gave me a beautiful flower.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 23,
+      word: 'ball',
+      translation: 'pelota',
+      example: 'The kids are playing with a ball.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 24,
+      word: 'teacher',
+      translation: 'profesor',
+      example: 'Our teacher is very helpful.',
+      image: './assets/images/house.png'
+    },
+    {
+      id: 25,
+      word: 'movie',
+      translation: 'película',
+      example: 'We watched a great movie last night.',
+      image: './assets/images/house.png'
+    }
+]
